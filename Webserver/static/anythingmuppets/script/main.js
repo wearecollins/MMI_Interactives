07e865c12e45832166cd9b7c784e6800cd59ed6c
Brett Renfer <brett@robotconscience.com>
2016-04-10 12:10:35 -0400

    Initial pass at transitions

--- Webserver/static/anythingmuppets/script/main.js
+++ Webserver/static/anythingmuppets/script/main.js
@@ -32,27 +32,44 @@ var script = function(/*manager*/){
 
 	};
 
+	var openClasses = "parchBg bottomCenteredContainer fillAbs";
+	var spinClasses = "parchBg bottomCenteredContainer fillAbs";
+
 	function showOpenPrompt(){
 		var openDiv = document.getElementById("promptOpenDrawers");
-		show(openDiv);
-
-		hide( document.getElementById("start") );
-		show( document.getElementById("done") );
+		// show(openDiv);
+		openDiv.className = openClasses+" visible";
+		setTimeout(function(){
+			hide( document.getElementById("start") );
+			show( document.getElementById("done") );
+		}, 1000);
 	}
 
 	function hideOpenPrompt(){
 		var openDiv = document.getElementById("promptOpenDrawers");
-		hide(openDiv);
+		// hide(openDiv);
+		openDiv.className = openClasses + " hideMe";
+
+		setTimeout(function(){
+			openDiv.className = openClasses;
+		}, 1000);
 	}
 
 	function showSpinPrompt(){
 		var spinDiv = document.getElementById("promptSpinPuppet");
-		show(spinDiv);
+		// show(spinDiv);
+		spinDiv.className = spinClasses+" visible";
 	}
 
 	function hideSpinPrompt(){
 		var spinDiv = document.getElementById("promptSpinPuppet");
-		hide(spinDiv);
+		// hide(spinDiv);
+		
+		spinDiv.className = spinClasses + " hideMe";
+
+		setTimeout(function(){
+			spinDiv.className = spinClasses;
+		}, 1000);
 
 		show( document.getElementById("start") );
 		hide( document.getElementById("done") );
@@ -62,6 +79,7 @@ var script = function(/*manager*/){
 		currentScript.style.visibility = "hidden";
 		currentScript.style.display = "none";
 		hideSpinPrompt();
+		hideOpenPrompt();
 
 		//cleanup
 		window.removeEventListener("start", showOpenPrompt.bind(this));
