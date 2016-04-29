d823159ed1593092bf576339ea4ed98d885672e9
Quin Kennedy <quinkennedy@gmail.com>
2016-04-29 07:52:45 -0700

    unbroke serving of static dir

--- Webserver/server.js
+++ Webserver/server.js
@@ -15,6 +15,8 @@ var WebSocket = require(Path.join(__dirname, 'websockets.js'));
 var RemoteLogs = require(Path.join(__dirname, 'log4javascript_to_log4js.js'));
 var DirLister = require(Path.join(__dirname, 'dirlister.js'));
 
+// set up logging
+Logger.configure(require('./log4js_conf.json'));
 var logger = Logger.getLogger('Server');
 
 /**
@@ -28,11 +30,9 @@ for (var argI = 0; argI < argv.length; argI++){
   switch(argv[argI]){
     case '--station':
       station = argv[++argI];
+      logger.debug('configured as station:',station);
   }
 }
-
-// set up logging
-Logger.configure(require('./log4js_conf.json'));
 
 /**
  * handles persistent configs
@@ -44,17 +44,23 @@ config.load('static/'+station+'/config.json').
   catch( reason => 
       logger.error('could not load configuration file', reason) ).
   then( function resolved(){
+    logger.debug('[resolved] config done loading');
     // set up HTTP handlers
     /**
      * Express server for simple middleware-based message handling
      * @type {Express}
      */
     var app = express();
+    app.use(Logger.connectLogger(
+              Logger.getLogger('express', {level: 'auto'})));
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(RemoteLogs.log(Logger));
     {
       //create full webpath <-> filepath mapping for directory lister
-      var staticList = config.getConfig('additionalStatic') || [];
+      //JSON dance to achieve deep-copy
+      var staticList = JSON.parse(
+                         JSON.stringify(
+                           config.getConfig('additionalStatic') || []));
       staticList.push({diskPath: Path.join(__dirname, 'static'),
                        webPath:  '/'});
       app.use('/list', DirLister.list(staticList));
@@ -64,6 +70,8 @@ config.load('static/'+station+'/config.json').
       var statics = config.getConfig('additionalStatic');
       if (statics){
         statics.forEach(function(item){
+          logger.debug('[resolved] setting up static hosting of'
+                       ,item.diskPath,'via',item.webPath);
           app.use(item.webPath, express.static(item.diskPath));
         });
       }
