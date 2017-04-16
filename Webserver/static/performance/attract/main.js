900deef19a00354bcf4ebb13798b3b5217e322d1
Quin Kennedy <quinkennedy@gmail.com>
2017-04-16 11:14:45 -0700

    Keeping one video for Performance attract

--- Webserver/static/performance/attract/main.js
+++ Webserver/static/performance/attract/main.js
@@ -1,17 +1,6 @@
 var attract = function( data, configHandler ){
   var currentAttractVideo = '';
 
-  /**
-   * Stores the recorded video in the persistent config
-   */
-  function loadNewAttract(e){
-    currentAttractVideo = 'output/performance/' + e.detail;
-    configHandler.set({'attractVideo':currentAttractVideo});
-  }
-
-  // Whenever a new video is recorded, store it in the config
-  window.addEventListener('videoRecorded', loadNewAttract);
-
   var videoDiv;
 
   this.enter = function(/*evt*/){
