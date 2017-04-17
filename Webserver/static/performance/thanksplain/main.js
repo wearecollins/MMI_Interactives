93dc7394aeda10189bba2563e889d3dfaf212435
Quin Kennedy <quinkennedy@gmail.com>
2017-04-16 20:23:40 -0700

    Added thanks screen timeout to admin
    Thanks screens get their timeout value from the global config.
    These can be edited from the admin screen.
    
    Also made admin screen more templated and removed special handling.

--- Webserver/static/performance/thanksplain/main.js
+++ Webserver/static/performance/thanksplain/main.js
@@ -1,4 +1,33 @@
-var thanksplain = function(/* data, configHandler */){
-  this.enter = function enter(){};
-  this.exit = function exit(){};
+var thanksplain = function(data, configHandler){
+
+  var nextTimeout;
+
+  this.enter = function(/*evt*/){
+
+    var timeoutTime = getTimeout(5000);
+
+    //auto-next after short delay
+    nextTimeout = setTimeout(function(){
+      window.events.dispatchEvent(new Event('next'));
+    }, timeoutTime);
+  };
+
+  /**
+   *  Get this page's timeout based on what is configured
+   *  @param {number} default 
+   *                  The default timeout to use if nothing is configured
+   */
+  function getTimeout(time){
+    var cTime = configHandler.get('thanksTimeout', 0);
+    if (cTime){
+      //convert from seconds to millis
+      return cTime * 1000;
+    } else {
+      return time;
+    }
+  }
+
+  this.exit = function(/*evt*/){
+    clearTimeout(nextTimeout);
+  };
 };
